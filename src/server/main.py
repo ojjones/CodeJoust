011f73b42f865cf997f5787dae9174b8fa700de9
Oliver Jones <ojones@isilon.com>
2014-06-12 15:04:48 -0700

    Adding WebSockets

--- src/server/main.py
+++ src/server/main.py
@@ -1,9 +1,21 @@
-import os.path
+import os
 import cherrypy
 import json
 from cherrypy.lib.static import serve_file
+
+import random
+from ws4py.server.cherrypyserver import WebSocketPlugin, WebSocketTool
+from ws4py.websocket import WebSocket
+from ws4py.messaging import TextMessage
  
 current_dir = os.path.abspath('../www')
+
+class ChatWebSocketHandler(WebSocket):
+    def received_message(self, m):
+        cherrypy.engine.publish('websocket-broadcast', m)
+
+    def closed(self, code, reason="A client left the room without a proper explanation."):
+        cherrypy.engine.publish('websocket-broadcast', TextMessage(reason))
 
 config = {
     '/': {
@@ -17,6 +29,10 @@ config = {
         'tools.json_in.on': True,
         'tools.response_headers.on': True,
         'tools.response_headers.headers': [('Content-Type', 'application/json')]
+        },
+    '/ws': {
+        'tools.websocket.on': True,
+        'tools.websocket.handler_cls': ChatWebSocketHandler
         }
      }
 
@@ -24,6 +40,10 @@ class Root():
     exposed = True
     game_id = 0
     current_games = {}
+
+    @cherrypy.expose
+    def ws(self):
+        cherrypy.log("Handler created: %s" % repr(cherrypy.request.ws_handler))
 
     @cherrypy.tools.accept(media='text/plain')
     def GET(self):
@@ -47,4 +67,13 @@ class Root():
     def DELETE(self):
         cherrypy.session.pop('mystring', None)
 
-cherrypy.quickstart(Root(), '/', config=config)
+if __name__ == '__main__':
+    cherrypy.config.update({
+        'server.socket_host': '0.0.0.0',
+        'server.socket_port': 9000
+    })
+
+    WebSocketPlugin(cherrypy.engine).subscribe()
+    cherrypy.tools.websocket = WebSocketTool()
+
+    cherrypy.quickstart(Root(), '/', config=config)
