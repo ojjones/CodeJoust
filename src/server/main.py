59b4ce7a060fe7254a26e0437146896b6a3cfb7a
Oliver Jones <ojones@isilon.com>
2014-06-12 14:59:29 -0700

    Adding Game Id

--- src/server/main.py
+++ src/server/main.py
@@ -1,5 +1,6 @@
 import os.path
 import cherrypy
+import json
 from cherrypy.lib.static import serve_file
  
 current_dir = os.path.abspath('../www')
@@ -15,22 +16,30 @@ config = {
         'tools.sessions.on': True,
         'tools.json_in.on': True,
         'tools.response_headers.on': True,
-        'tools.response_headers.headers': [('Content-Type', 'text/plain')]
+        'tools.response_headers.headers': [('Content-Type', 'application/json')]
         }
      }
 
 class Root():
     exposed = True
+    game_id = 0
+    current_games = {}
 
     @cherrypy.tools.accept(media='text/plain')
     def GET(self):
         return cherrypy.session['mystring']
 
     @cherrypy.tools.json_in()
-    def POST(self, problem):
-        test = cherrypy.request.json
-        print test
-        return test
+    def POST(self, vpath):
+        if vpath == 'setup':
+            tmpGameId = self.game_id
+            self.game_id += 1
+            problem = cherrypy.request.json
+            self.current_games[tmpGameId] = {'Problem':problem['problem']}
+            print self.current_games
+            return json.dumps({'game_id':tmpGameId})
+
+        return vpath
 
     def PUT(self, another_string):
         cherrypy.session['mystring'] = another_string
