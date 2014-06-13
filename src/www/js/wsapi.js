e498fa7ff722ce03c8f672f334d78b588cfda94c
wchristie <wade.christie@isilon.com>
2014-06-12 21:31:23 -0700

    fucking shit up

--- src/www/js/wsapi.js
+++ src/www/js/wsapi.js
@@ -3,26 +3,26 @@
 
 	wsapi = {};
 
-	wsapi.INIT_REQ_ID = 0;
-	wsapi.INIT_RES_ID = 1;
+	wsapi.TEAM_INIT_REQ_ID = 0;
+	wsapi.TEAM_INIT_RES_ID = 1;
 	wsapi.TEAM_CODE_PUSH_ID = 2;
-	wsapi.EDITOR_UPDATE = 3;
-	wsapi.SCORE_INIT = 4;
-	wsapi.START = 5;
-	wsapi.STOP = 6;
-	wsapi.WINNER = 7;
+	wsapi.SCORE_EDITOR_UPDATE_ID = 3;
+	wsapi.SCORE_INIT_ID = 4;
+	wsapi.START_ID = 5;
+	wsapi.STOP_ID = 6;
+	wsapi.WINNER_ID = 7;
 
 	wsapi.INIT_REQ = function(gameid, teamid) {
 		return {
-			type: wsapi.INIT_REQ_ID,
+			type: wsapi.TEAM_INIT_REQ_ID,
 			game_id: gameid,
 			team_id: teamid
 		};
 	};
 
-	wsapi.INIT_SCORE = function(gameid, teamid) {
+	wsapi.SCORE_INIT = function(gameid, teamid) {
 		return {
-			type: wsapi.SCORE_INIT,
+			type: wsapi.SCORE_INIT_ID,
 			game_id: gameid,
 		};
 	};
