60ee90652d3b73e018ab1d066574928fc9fdae21
wchristie <wade.christie@isilon.com>
2014-06-12 17:16:38 -0700

    added TEAM_CODE_PUSH rpc

--- src/www/js/wsapi.js
+++ src/www/js/wsapi.js
@@ -6,11 +6,22 @@
 	wsapi.INIT_REQ_ID = 0;
 	wsapi.INIT_RES_ID = 1;
 
+	wsapi.TEAM_CODE_PUSH_ID = 2;
+
 	wsapi.INIT_REQ = function(gameid, teamid) {
 		return {
 			type: wsapi.INIT_REQ_ID,
 			game_id: gameid,
 			team_id: teamid
+		};
+	};
+
+	wsapi.TEAM_CODE_PUSH = function(gameid, teamid, code) {
+		return {
+			type: wsapi.TEAM_CODE_PUSH_ID,
+			game_id: gameid,
+			team_id: teamid,
+			code: code
 		};
 	};
 
