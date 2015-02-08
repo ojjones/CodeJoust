dae1c0c215d855f85d92232279368481b2ecac69
Wade Christie <wade.christie@gmail.com>
2015-02-07 17:24:15 -0800

    String literals don't need to be regex compiled

--- src/main.py
+++ src/main.py
@@ -18,16 +18,16 @@ define("debug", default=False, help="run in debug mode")
 def make_app():
     return tornado.web.Application(
         [
-            url(r"/", controllers.DefaultHandler),
-            url(r"/newgame", controllers.NewGameHandler, name="newgame"),
-            url(r"/api/joingame", controllers.JoinGameHandler, name="joingame"),
+            url("/", controllers.DefaultHandler),
+            url("/newgame", controllers.NewGameHandler, name="newgame"),
+            url("/api/joingame", controllers.JoinGameHandler, name="joingame"),
             url(r"/overlord/g([0-9]+)", controllers.OverlordHandler, name="overlord"),
-            url(r"/overlord/socket", controllers.OverlordSocketHandler, name="overlord_socket"),
+            url("/overlord/socket", controllers.OverlordSocketHandler, name="overlord_socket"),
             url(r"/game/g([0-9]+)p(1|2)", controllers.GameHandler, name="game"),
-            url(r"/game/socket", controllers.GameSocketHandler, name="game_socket"),
+            url("/game/socket", controllers.GameSocketHandler, name="game_socket"),
             url(r"/api/problem/(.+)", problems.ProblemHandler, name="api_problem"),
             url("/api/problems", problems.ProblemHandler, name="api_problems"),
-            url(r"/api/compile", controllers.CompileHandler, name="compile"),
+            url("/api/compile", controllers.CompileHandler, name="compile"),
         ],
         debug=options.debug,
         static_path=STATICPATH,
