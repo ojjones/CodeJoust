f8d2f5dec57a1242b97c9669ab81ddb76acaf125
Oliver Jones <oliverjones@Laurens-MacBook-Pro.local>
2015-02-07 17:20:27 -0800

    Removing accidental add to problems.py

--- src/problems.py
+++ src/problems.py
@@ -84,18 +84,3 @@ class ProblemHandler(controllers.BaseApiHandler):
         except ProblemNotFoundError as err:
             raise tornado.web.HTTPError(404, err.message)
 
-class JoustProblem:
-    def __init__(self, problem_name):
-        # XXXJDR make this cleaner, secure, etc...
-        self.reference_input = "../problems/%s/sample_input" % problem_name
-        self.reference_output = "../problems/%s/sample_output" % problem_name
-    def make_compile_cmd(self, filename):
-        return
-    def make_binary_name(self, filename):
-        return
-
-class CJoustProblem(JoustProblem):
-    def make_binary_name(self, filename):
-        return "%s.out" % filename
-    def make_compile_cmd(self, filename):
-        return "gcc %s -o %s" % (filename, self.make_binary_name(filename))
